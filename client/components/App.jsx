b097da438c943b02fd9c16bbc42c22217d1871fe
Max Proano <proanom89@gmail.com>
2021-04-11 16:25:01 -0700

    Setup webpack and react

---
+++ client/components/App.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <h1>More Places to Stay</h1>
+  }
+
+}
+
+export default App;
